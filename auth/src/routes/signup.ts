5073ba2315e019731993989ca29b9f4f77273ace
LalitKushwah <lalitkushwah52525@gmail.com>
2021-09-16 09:24:04 +0530

    implemented hashed password

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,34 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-errors';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-    
-    res.send({ working: true, signup: true });
+router.post('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password must be between 4 and 20 char')
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+    }
+    const { email, password } = req.body;
+    const isExistingUser = await User.findOne({ email });
+    if (isExistingUser) {
+        console.error('User already existed');
+        throw new BadRequestError('User already existed')
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+    res.status(201).send({ user })
 });
 
 export { router as signupRouter };
